d457d7b0cd6e02d6e074d9727100147662b689ba
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-08-26 16:32:16 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, Extension, find_packages
+
+c_extension = Extension(
+    'pyinspect.pyinspect_core',
+    sources=['pyinspect/pyinspect_core.c'],
+)
+
+setup(
+    name="Dan-PyInspect",
+    version="5.0.0",
+    packages=find_packages(),
+    ext_modules=[c_extension],
+    zip-safe=False,
+)
